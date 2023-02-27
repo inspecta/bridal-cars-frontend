4109e66904c80533f5175d5c4b6598d923b7f9ee
Fikerte-T <tofikertesfaye@gmail.com>
2023-02-27 19:57:29 +0300

    Add Login and Signup components

---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <h1>login form</h1>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/components/Signup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+      <h1>signup form</h1>
+    </div>
+  )
+}
+
+export default Signup
