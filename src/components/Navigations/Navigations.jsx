9185c5b88fcd0d3315b155e800fe23a6321bb89c
LIBERCOSOFT <akinspreciousola@gmail.com>
2023-03-01 20:43:19 +0100

    Implement sidenav responsiveness

--- src/components/Navigations/Navigations.jsx
+++ src/components/Navigations/Navigations.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable jsx-quotes */
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import {
   FaFacebookF,
@@ -10,35 +10,57 @@ import {
   FaPinterestP,
   FaCopyright,
 } from 'react-icons/fa';
+import { GiHamburgerMenu, GiCancel } from 'react-icons/gi';
 
-const Navigation = () => (
-  <>
-    <Nav className="nav_container">
-      <h3 className="nav_logo">
-        Bridal Car <br /> Rental
-      </h3>
-      <div className="nav_links">
-        <Nav.Link href="#">LIST ALL CARS</Nav.Link>
-        <Nav.Link href="#1">RESERVE CAR</Nav.Link>
-        <Nav.Link href="#2">ADD CAR</Nav.Link>
-        <Nav.Link href="#3">DELETE CAR</Nav.Link>
-        <Nav.Link href="#4">LIST ALL RESERVATIONS</Nav.Link>
-      </div>
-      <div className="nav_socials_container">
-        <div className="nav_socials">
-          <FaFacebookF />
-          <FaGithub />
-          <FaTwitter />
-          <FaGooglePlusG />
-          <FaPinterestP />
+const Navigation = () => {
+  const [sideNav, setSideNav] = useState(false);
+  return (
+    <>
+      {sideNav === false && (
+        <button
+          type="button"
+          className="hamburger_menu"
+          onClick={() => setSideNav('show_nav')}
+        >
+          <GiHamburgerMenu />
+        </button>
+      )}
+      <Nav className={`nav_container ${sideNav}`}>
+        {sideNav === 'show_nav' && (
+          <button
+            type="button"
+            className="cancel_menu"
+            onClick={() => setSideNav(false)}
+          >
+            <GiCancel />
+          </button>
+        )}
+        <h3 className="nav_logo">
+          Bridal Car <br /> Rental
+        </h3>
+        <div className="nav_links">
+          <Nav.Link href="#">LIST ALL CARS</Nav.Link>
+          <Nav.Link href="#1">RESERVE CAR</Nav.Link>
+          <Nav.Link href="#2">ADD CAR</Nav.Link>
+          <Nav.Link href="#3">DELETE CAR</Nav.Link>
+          <Nav.Link href="#4">LIST ALL RESERVATIONS</Nav.Link>
         </div>
-        <div className="nav_copyright">
-          <FaCopyright />
-          <span>Copyright 2023</span>
+        <div className="nav_socials_container">
+          <div className="nav_socials">
+            <FaFacebookF />
+            <FaGithub />
+            <FaTwitter />
+            <FaGooglePlusG />
+            <FaPinterestP />
+          </div>
+          <div className="nav_copyright">
+            <FaCopyright />
+            <span>Copyright 2023</span>
+          </div>
         </div>
-      </div>
-    </Nav>
-  </>
-);
+      </Nav>
+    </>
+  );
+};
 
 export default Navigation;
