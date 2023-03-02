fb6d0cfbb65bc3a7fbf7cecf222b1f4a13b0756b
Derrick Mulinde <djmulinde@gmail.com>
2023-03-02 12:14:25 +0300

    Add car components

---
+++ src/components/Car.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FaFacebookF, FaTwitter, FaLinkedin } from 'react-icons/fa';
+
+const Car = ({ car, onClick }) => (
+  <div
+    className="car-component"
+    role="button"
+    tabIndex="0"
+    onClick={onClick}
+    onKeyDown={(event) => {
+      if (event.key === 'Enter' || event.key === ' ') {
+        onClick();
+      }
+    }}
+    onTouchStart={onClick}
+    onTouchEnd={(event) => {
+      event.preventDefault();
+      onClick();
+    }}
+  >
+    <img
+      src={car.photo}
+      alt="bridal-car"
+      style={{ width: '450px' }}
+    />
+    <h1>{car.name}</h1>
+    <div className="car-component-info">
+      <p>{car.description}</p>
+      <div className="social-icons">
+        <span><FaFacebookF /></span>
+        <span><FaTwitter /></span>
+        <span><FaLinkedin /></span>
+      </div>
+    </div>
+  </div>
+);
+
+Car.propTypes = {
+  car: PropTypes.objectOf.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+export default Car;
---
+++ src/components/Cars.js
@@ -0,0 +1,99 @@
+import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useRef, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaCaretLeft, FaCaretRight } from 'react-icons/fa';
+import { fetchAllCars } from '../redux/features/carSlice';
+import Car from './Car';
+
+const Cars = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const getAllCars = useSelector((state) => state.cars);
+
+  useEffect(() => {
+    dispatch(fetchAllCars());
+  }, [dispatch]);
+
+  const handleClick = (carObject) => {
+    navigate(
+      '/car-details',
+      {
+        state: {
+          cars: carObject,
+        },
+      },
+    );
+  };
+
+  /*
+    Cars Scroll Animation
+  */
+  const [position, setPosition] = useState(0);
+  const scrollRef = useRef(null);
+
+  const handlePrevClick = () => {
+    if (position > 0) {
+      setPosition(position - 1);
+      scrollRef.current.scrollBy({
+        left: -380,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  const handleNextClick = () => {
+    if (position < getAllCars.cars.length - 1 && scrollRef.current) {
+      setPosition(position + 1);
+      scrollRef.current.scrollBy({
+        left: 380,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  const isAtBeginning = position === 0;
+  const isAtEnd = position === getAllCars.cars.length - 1;
+
+  return (
+    <div className="cars-container">
+      <h1>LATEST BRIDAL CARS</h1>
+      <p>Please select a bridal car</p>
+      <div className="car-list-wrapper">
+        <button
+          onClick={handlePrevClick}
+          disabled={isAtBeginning}
+          type="button"
+          className="btnPrev"
+        >
+          <FaCaretLeft />
+        </button>
+        <div className="cars-list" ref={scrollRef}>
+          {
+            getAllCars.cars.map((car) => (
+              <Car
+                key={car.id}
+                car={car}
+                onClick={() => handleClick(car)}
+                onKeyDown={() => handleClick()}
+              />
+            ))
+          }
+        </div>
+        <button
+          onClick={handleNextClick}
+          disabled={isAtEnd}
+          type="button"
+          className="btnNext"
+        >
+          <FaCaretRight />
+        </button>
+      </div>
+      <div className="">
+        <Link to="/add-car">Add Car</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Cars;
