405c3bcbbe160a590db354f52c625c145796b8e4
Derrick Mulinde <djmulinde@gmail.com>
2023-03-02 12:16:38 +0300

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import CarDetails from './components/CarDetails';
+import Cars from './components/Cars';
+import AddCar from './components/AddCar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Cars />} />
+        <Route path="/cars/:id" element={<CarDetails />} />
+        <Route path="/car-details" element={<CarDetails />} />
+        <Route path="/add-car" element={<AddCar />} />
+      </Routes>
     </div>
   );
 }
