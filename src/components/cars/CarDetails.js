5d763eacc930a4be94830bff1d219f09ef8647f4
Fikerte-T <tofikertesfaye@gmail.com>
2023-03-06 21:51:05 +0300

    Autofill car information in reservation form

--- src/components/cars/CarDetails.js
+++ src/components/cars/CarDetails.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate, useLocation, Link } from 'react-router-dom';
 import { FaAngleLeft } from 'react-icons/fa';
 
 const CarDetails = () => {
@@ -16,7 +16,9 @@ const CarDetails = () => {
     navigate('/');
   };
 
-  const handleReserve = () => true;
+  // const handleReserve = () => {
+  //   navigate('/reserve-car');
+  // };
 
   return (
     <>
@@ -45,7 +47,17 @@ const CarDetails = () => {
               {selectedCar.created_at}
             </p>
           </div>
-          <button type="button" onClick={() => handleReserve()}>Reserve</button>
+          {/* <button type="button" onClick={() => handleReserve(selectedCar)}>Reserve</button> */}
+          <div className="add-car-btn">
+            <Link
+              to="/reserve-car"
+              state={{
+                car: selectedCar,
+              }}
+            >
+              Reserve
+            </Link>
+          </div>
         </div>
       </div>
       <div style={{ textAlign: 'center' }}>
